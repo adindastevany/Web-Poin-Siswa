3811303c7f2d8e762d787089d602589bc6bdea82
Adinda Putri Stevany 18 <adindaputristevani@gmail.com>
2020-02-13 09:34:05 +0700

    first commit

---
+++ registrasi.php
@@ -0,0 +1,10 @@
+<?php include('functions.php') ?>
+//...
+
+<form method="post" action="register.php">
+	<?php echo display_error(); ?>
+//...
+</form>
+
+<input type="text" name="username" value="<?php echo $username; ?>">
+<input type="email" name="email" value="<?php echo $email; ?>">
